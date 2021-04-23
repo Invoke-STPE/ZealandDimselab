cb8e4bf4c9ec66166acdd11dc9483809bcc78273
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2021-04-23 21:48:39 +0200

    Merge with ItemService, added models.
    Merge so that I had access to unittest project.

---
+++ ZealandDimselab/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZealandDimselab.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public User(int id, string name, string email, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
