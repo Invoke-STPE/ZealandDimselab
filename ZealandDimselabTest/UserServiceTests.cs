17a0df7ad31606a517d6618b77db245a5903d38f
Christopher <38313657+NattyPew@users.noreply.github.com>
2021-04-29 10:13:40 +0200

    Fixed updating item

--- ZealandDimselabTest/UserServiceTests.cs
+++ ZealandDimselabTest/UserServiceTests.cs
@@ -8,7 +8,6 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using ZealandDimselab.Models;
-using ZealandDimselab.Repository;
 using ZealandDimselab.Services;
 
 namespace ZealandDimselabTest
