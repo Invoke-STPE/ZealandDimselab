363bcd089cd6bd870b1132bfc038aa987da63984
Christopher <38313657+NattyPew@users.noreply.github.com>
2021-05-21 11:11:15 +0200

    Added sort methods & buttons for all table columns

--- ZealandDimselab/Models/BookedItem.cs
+++ ZealandDimselab/Models/BookedItem.cs
@@ -34,36 +34,81 @@ namespace ZealandDimselab.Models
             return String.Compare(this.Item.Name, bi.Item.Name);
         }
 
-        private class sortItemIdAscendingHelper : IComparer
-        {
-            int IComparer.Compare(object a, object b)
-            {
-                BookedItem bi1 = (BookedItem)a;
-                BookedItem bi2 = (BookedItem)b;
 
-                if (bi1.Item.Id > bi2.Item.Id)
-                    return 1;
-                if (bi1.Item.Id < bi2.Item.Id)
-                    return -1;
+        //// Beginning of nested classes. Used for sorting BookedItems
+        //private class sortItemIdAscendingHelper : IComparer
+        //{
+        //    int IComparer.Compare(object a, object b)
+        //    {
+        //        BookedItem bi1 = (BookedItem)a;
+        //        BookedItem bi2 = (BookedItem)b;
 
-                return 0;
-            }
-        }
-        private class sortItemIdDescendingHelper : IComparer
-        {
-            int IComparer.Compare(object a, object b)
-            {
-                BookedItem bi1 = (BookedItem)a;
-                BookedItem bi2 = (BookedItem)b;
+        //        if (bi1.Item.Id > bi2.Item.Id)
+        //            return 1;
+        //        if (bi1.Item.Id < bi2.Item.Id)
+        //            return -1;
 
-                if (bi1.Item.Id > bi2.Item.Id)
-                    return 1;
-                if (bi1.Item.Id < bi2.Item.Id)
-                    return -1;
+        //        return 0;
+        //    }
+        //}
 
-                return 0;
-            }
-        }
+        //private class sortItemIdDescendingHelper : IComparer
+        //{
+        //    int IComparer.Compare(object a, object b)
+        //    {
+        //        BookedItem bi1 = (BookedItem)a;
+        //        BookedItem bi2 = (BookedItem)b;
 
+        //        if (bi1.Item.Id > bi2.Item.Id)
+        //            return 1;
+        //        if (bi1.Item.Id < bi2.Item.Id)
+        //            return -1;
+
+        //        return 0;
+        //    }
+        //}
+
+        //private class sortItemNameAscendingHelper : IComparer
+        //{
+        //    int IComparer.Compare(object a, object b)
+        //    {
+        //        BookedItem bi1 = (BookedItem)a;
+        //        BookedItem bi2 = (BookedItem)b;
+
+        //        return String.Compare(bi1.Item.Name, bi2.Item.Name);
+        //    }
+        //}
+
+        //private class sortItemNameDescendingHelper : IComparer
+        //{
+        //    int IComparer.Compare(object a, object b)
+        //    {
+        //        BookedItem bi1 = (BookedItem)a;
+        //        BookedItem bi2 = (BookedItem)b;
+
+        //        return String.Compare(bi2.Item.Name, bi1.Item.Name);
+        //    }
+        //}
+        //// End of nested classes
+
+        //public static IComparer sortItemIdAscending()
+        //{
+        //    return (IComparer) new sortItemIdAscendingHelper();
+        //}
+
+        //public static IComparer sortItemIdDescending()
+        //{
+        //    return (IComparer)new sortItemIdDescendingHelper();
+        //}
+
+        //public static IComparer sortItemNameAscending()
+        //{
+        //    return (IComparer)new sortItemNameAscendingHelper();
+        //}
+
+        //public static IComparer sortItemNameDescending()
+        //{
+        //    return (IComparer)new sortItemNameDescendingHelper();
+        //}
     }
 }
