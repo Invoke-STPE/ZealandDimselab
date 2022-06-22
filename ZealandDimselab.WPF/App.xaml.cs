9cf5de58d384c946a6c6f396c5bbfa07ef5112f7
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-06-22 10:27:14 +0200

    Project Setup & DI
    Created project and added Dependency Injection.

---
+++ ZealandDimselab.WPF/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZealandDimselab.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            
+        }
+
+        private void OnStartup(object sender, StartupEventArgs eventArgs)
+        {
+
+        }
+    }
+}
